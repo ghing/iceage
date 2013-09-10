2c7bf6a084356cd0a74986a0e69fcf178f391504
Geoff Hing <geoffhing@gmail.com>
2013-09-10 14:24:06 -0500

    Show individual transfers in a modal popup

--- js/views/CharacterTransferView.js
+++ js/views/CharacterTransferView.js
@@ -1,30 +1,29 @@
 define([
+  'underscore',
   'backbone',
   'd3',
-  'bootstrap'
-], function(Backbone, d3, bootstrap)  {
-  return Backbone.View.extend({
+  'bootstrap',
+  'views/StateViewMixin',
+  'views/FacilitiesViewMixin',
+  'views/TransferViewMixin'
+], function(_, Backbone, d3, bootstrap, StateViewMixin, FacilitiesViewMixin, TransferViewMixin)  {
+  return Backbone.View.extend(_.extend({}, StateViewMixin, FacilitiesViewMixin, TransferViewMixin, {
     options: {
+      pointSize: 5,
       projection: d3.geo.albersUsa,
-      scale: 1200
+      scale: 1200,
+      'stroke-width': 1,
+      'stroke': 'blue'
     },
 
     initialize: function() {
       this.characterEvents = this.options.characterEvents;
       this.facilities = this.options.facilities;
       this.states = this.options.states;
-      /*
-      var width = this.$el.width();
-      var height = this.$el.height();
-      var projection = this.options.projection()
-          .scale(this.options.scale)
-          .translate([width / 2, height / 2]);
+      this.$containerEl = this.$('.modal-body');
+      this._statesRendered = false;
 
-      this.path = d3.geo.path()
-          .projection(projection);
-      this.d3El = d3.select(this.$el.selector).insert("svg:svg")
-          .attr("width", width)
-          .attr("height", height);
+      this.d3El = d3.select(this.$containerEl.selector).insert("svg:svg");
 
       this.statesLayer = this.d3El.append("svg:g")
           .attr("id", "character-states");
@@ -35,8 +34,6 @@ define([
       this.transfersLayer = this.d3El.append("svg:g")
           .attr("id", "character-transfers");
 
-      this.states.on('sync', this.renderStates, this);
-      */
       Backbone.on('transfer', this.setTransfer, this);
 
       this.$el.modal({
@@ -44,30 +41,83 @@ define([
       });
     },
 
+    // Get the width and height of the container element
+    //
+    // We have to do some hacking since it's initially hidden
+    //
+    // Based on http://www.foliotek.com/devblog/getting-the-width-of-a-hidden-element-with-jquery-using-width/
+    getHiddenDimensions: function() {
+      var $el = this.$containerEl;
+      var $hiddenParents = $el.parents().andSelf().not(':visible');
+      var props = { position: 'absolute', visibility: 'hidden', display: 'block' };
+      var oldProps = [];
+      var dims = {};
+      var name;
+
+      $hiddenParents.each(function () {
+        var old = {};
+
+        for (name in props) {
+          old[name] = $el.css(name);
+          $el.css(name, props[name]);
+        }
+
+        oldProps.push(old);
+      });
+
+      dims.width = $el.width();
+      dims.height = $el.height();
+
+      $hiddenParents.each(function(i) {
+        var old = oldProps[i];
+
+        for (name in props) {
+          // For our use case, we can just unset the CSS property rather than
+          // restoring the old version
+          $el.css(name, '');
+        }
+      });
+
+      return dims;
+    },
+
     setTransfer: function(date, tracId) {
+      var view = this;
       var transfer = this.characterEvents.where({
         tracId: tracId,
         date: date.format("YYYY-MM-DD")
       })[0];
-      var fromFacility = transfer.get('from_facility');
-      var toFacility = transfer.get('to_facility');
-      if (fromFacility) {
-        fromFacility = this.facilities.get(fromFacility);
-      }
-      if (toFacility) {
-        toFacility = this.facilities.get(toFacility);
-      }
-      // BOOKMARK
-      // TODO: Display individual transfer
-      this.$el.modal('show');
-    },
+      var fromFacilityId = transfer.get('from_facility');
+      var toFacilityId = transfer.get('to_facility');
+      var facilities = this.facilities.filterCollection(function(facility) {
+        return facility.id === fromFacilityId || facility.id === toFacilityId; 
+      });
+      var transfers = [[fromFacilityId, toFacilityId]];
 
-    renderStates: function() {
-      return this;
+      // Defer rendering until the modal is shown
+      this.$el.on('shown', function() {
+        view.$el.off('shown');
+        view.width = view.$containerEl.width();
+        view.height = view.$containerEl.height();
+        view.projection = view.options.projection()
+            .scale(view.options.scale)
+            .translate([view.width / 2, view.height / 2]);
+        view.d3El.attr("width", view.width)
+          .attr("height", view.height);
+        view.path = d3.geo.path()
+            .projection(view.projection);
+
+        view.renderStates(view.statesLayer, view.states, view.path);
+        view.renderFacilities(view.facilitiesLayer, facilities, view.path);
+        if (fromFacilityId && toFacilityId) {
+          view.renderTransfers(view.transfersLayer, transfers, facilities, view.path, true);
+        }
+      });
+      this.$el.modal('show');
     },
 
     render: function() {
       return this;
     }
-  });
+  }));
 });
--- js/views/StateViewMixin.js
+++ js/views/StateViewMixin.js
@@ -4,6 +4,14 @@ define([
 ], function(Backbone, d3) {
   return {
     renderStates: function(el, states, path) {
+      // Defer rendering until there's actually data
+      if (states.length === 0) {
+        states.once('sync', function() {
+          this.renderStates(el, states, path);
+        }, this);
+        return this;
+      }
+
       el.selectAll("path")
         .data(states.toJSON())
         .enter().append("svg:path")
